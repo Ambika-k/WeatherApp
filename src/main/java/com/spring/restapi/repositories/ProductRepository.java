9da1a7895158c9bf4b4b2a223a27346a85c2f561
Ambika-k <ambikakumpatlas44@gmail.com>
2022-09-01 18:33:55 +0530

    first commit

---
+++ src/main/java/com/spring/restapi/repositories/ProductRepository.java
@@ -0,0 +1,19 @@
+/*
+ * package com.spring.restapi.repositories;
+ * 
+ * import java.util.Optional;
+ * 
+ * import org.springframework.data.mongodb.repository.MongoRepository;
+ * 
+ * import com.spring.restapi.models.Product;
+ * 
+ *//**
+	 * @author Vijaya Bramarambika August30, 2022
+	 * 
+	 *//*
+		 * 
+		 * 
+		 * 
+		 * public interface ProductRepository extends MongoRepository<Product, String> {
+		 * public Optional<Product> findById(String id); }
+		 */
